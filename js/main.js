978d1d64b11743b314a499456f2b69acaa520fb6
TheObscureKing <theobscureking90@gmail.com>
2023-11-09 15:49:38 +0100

    Esercizio Base completato

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/* alert("hello World!");
+
+document.writeln("<h3>Ciao Mondo!</h3>")
+
+document.getElementById("title_page").innerHTML = "Ciao Mondo!";
+ */
+
+//Variabili
+let name = prompt("Qual'è il tuo Nome?")
+
+let surname = prompt("Qual'è il tuo Cognome?")
+
+let favoriteColor = prompt("Qual'è il tuo Colore Preferito?")
+
+let numberOne = prompt("Scrivi un numero")
+let numberTwo = prompt("Scrivi un altro numero")
+
+let numbersDivision = numberOne / numberTwo
+
+
+//
+alert(`Il tuo nome è: ${name} \nIl tuo cognome è: ${surname} \nIl tuo Colore Preferito è: ${favoriteColor} \n${numberOne} : ${numberTwo} = ${numbersDivision} \n\nQuindi...`)
+
+alert(`...la tua password sarà: ${name}${surname}${favoriteColor}${numbersDivision}`)
+
